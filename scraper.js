555453b05864ae046565ebd7f12dcb859fd44a4b
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-06-12 00:11:24 +0100

    comments updated

--- scraper.js
+++ scraper.js
@@ -1,3 +1,11 @@
+// TO DO:
+// 1. must simulate user clicks on recaptcha iframes, if captcha is visible.
+// 2. scrape desination page and map through test centers to see if dates are found/not found
+// 3. make app run on a schdule, a few times a day, exept when servers are closed.
+// 4. communiate availability result to user, screen shot / mapped data of each test center and wether they have slots available
+
+// EXTRA FEATURE: if slot is available, make the bot book the next available slot for you. At present you ould have to login-in yourself ASAP and hope the slot is still available. 
+
 import dotenv from 'dotenv'
 import puppeteer from 'puppeteer-extra'
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
@@ -98,13 +106,3 @@ puppeteer.launch(chromeOptions).then(async browser => {
     console.log(err)
   }
 })
-
-// ? TO DO:
-// * must simulate user clicks on captcha feilds, if captcha is visible.
-// 1 scrape desination page and map through test centers to see if dates are found/not found
-// 1.1 use dev tools to simlate a test slot being avilable
-
-// 2 make app run on a schdule, a few times a day, exept when servers are closed.
-// 3 communiate availability result to user, screen shot / mapped data of each test center and wether they have slots available
-
-// EXTRA FEATURE: if slot is avaviable, make the bot book the next available slot for you. At present you ould have to login-in yourself ASAP and hope the slot is still available. 
